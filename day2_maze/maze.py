c8e1ba65f7050040ac4dd154c706ba15f14af44b
skpai <suresh.sasidharan-pillai@le-campus-numerique.fr>
2022-05-04 11:20:41 +0200

    maze

--- day2_maze/maze.py
+++ day2_maze/maze.py
@@ -8,17 +8,25 @@ class Maze:
         self.cols = cols
         self.target_positions = target_positions
         self.start_pos = start_pos
-        self.reward_matrix = -1+np.zeros(self.rows, self.cols)
+        self.reward_matrix = -1+np.zeros((rows, cols))
         if target_positions:
             for position in target_positions:
                 self.reward_matrix[position] = 100
     
     def check_start_pos(self):
         return self.start_pos not in self.target_positions  
+
+    def plot_maze(self):
+        plt.imshow(self.reward_matrix)
+        plt.grid()
+        plt.show()
+
     
 
 def main():
     maze = Maze(rows=5, cols=5, target_positions=[(0,0), (4,3)])
+    maze.plot_maze()
+    
 
 if __name__ == '__main__':
     main()

bb57768b2563d44e2e7139e155acef52fe71565a
audrey-feuvrier <audrey.feuvrier@gmail.com>
2022-05-05 11:45:21 +0200

    update agent with comments

--- day3/agent.py
+++ day3/agent.py
@@ -1,3 +1,6 @@
+"""
+Agent class for the SARSA algorithm.
+"""
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
@@ -6,10 +9,32 @@ import random
 from time import * 
 from visualizer import *
 
-
 class Agent:
+    '''
+    The agent class allow us to define the moving agent in the maze game i.e.
+    define the decision it is going to take which ends up having consequence 
+    in the environment (maze here).
+    '''
     def __init__(self, maze, epsilon, gamma, alpha, 
                  state, q_table, actions=['up', 'down', 'left', 'right']):
+        """
+        Initialize the agent.
+
+        Parameters
+        ----------
+        maze : maze.Maze
+            The environment to interact with.
+        epsilon : int      
+        gamma : int
+        alpha : int
+        state : tuple
+            The state in which we initialize the game, (0,0).
+        actions : list(str)
+            The 4 possible actions.
+        q_table : array
+            Politic we choose. Defined in the maze.
+            
+        """
         self.maze=maze
         self.epsilon = epsilon 
         self.gamma = gamma  
@@ -19,15 +44,34 @@ class Agent:
         self.q_table = q_table 
     
     def choose_action(self):
+        """
+        Choose an action with e-greedy.
+
+        Returns
+        -------
+        action : int
+            The action to take.
+        """
         state_actions = self.q_table[self.state]
         randomRate = np.random.uniform()
         if randomRate > self.epsilon:
             return self.actions.index(np.random.choice(self.actions)) 
-
         else:
             return state_actions.argmax()
     
     def update_q_table(self, reward, action, nxt_state):
+        """
+        Update the agent's knowledge of the environment.
+
+        Parameters
+        ----------
+        action : int
+            The action taken.
+        reward : float
+            The reward received.
+        nxt_state : tuple
+            Next position
+        """
         q_predict = self.q_table[self.state][action]
         if nxt_state in ['win', 'ghost'] :
             q_target = reward
@@ -38,6 +82,16 @@ class Agent:
 
 
 def path(state, is_terminated):
+    """
+    Printing the path as the agent moves.
+
+    Parameters
+    ----------
+    state : tuple
+        Current state.
+    is_terminated : bool
+        Boolean which is True when the game is over.
+    """
     print(state, end='')
     if not is_terminated: print(' > ', end='')
 
@@ -96,7 +150,8 @@ def main():
     EPSILON = 0.9
     GAMMA = 0.9
     ALPHA = 0.1
-    target = [(3,1), (4,4)]
+    # target = [(3,1), (4,4)]
+    target = [(4,4)]
     t_reward = 100
     f_reward_list = -1+np.zeros(SIZE)
     maze = Maze(ACTIONS)
@@ -124,7 +179,7 @@ def main():
                 is_terminated = True
                 nxt_state = (0,0)
             path(agent.state, is_terminated)
-            animate(agent.state, maze.map, ax)
+            # animate(agent.state, maze.map, ax)
             agent.state = nxt_state
             count +=1
             #time.sleep(0.05)
